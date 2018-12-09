5dfbd189be7582c19e406420696636d8b0dc3740
Jesús Sánchez de Lechina Tejada <jesus_38b@hotmail.com>
2018-12-09 11:55:21 +0100

    Creados objetos 3D sobre el mapa que sirvan para interactuar con ellos y cambiar de escena #4 #5
    Ahora mismo hay un ejemplo de cómo cambia de escena. Todo está en el script selectableObject del objeto "capsula" que he puesto en el mapa.

---
+++ Assets/Musemium/Scripts/selectableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class selectableObject : MonoBehaviour {
+	private Transform transform;
+	private CapsuleCollider collider;
+
+	// Use this for initialization
+	void Start () {
+		transform = this.GetComponent<Transform> ();
+		collider = this.GetComponent<CapsuleCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 rotationAngle = new Vector3 (1, 0, 1);
+		transform.Rotate (0, 45 * Time.deltaTime, 0, Space.World); // rotation in degrees per second
+	}
+
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log ("Colision detectada, cambiando de escena");
+		SceneManager.LoadScene("testScene");
+	}
+}
