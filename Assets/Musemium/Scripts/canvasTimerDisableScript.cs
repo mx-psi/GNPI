a2ba6c08b19db5d17e1e36403432a3ff297c5f4a
Jesús Sánchez de Lechina Tejada <jesus_38b@hotmail.com>
2018-12-13 10:49:14 +0100

    Nueva animación!! Animación para la escena musical
    - Añadidos los assets correspondientes
    - Es muy bonita
    - Usa mucha interpolación lineal

---
+++ Assets/Musemium/Scripts/canvasTimerDisableScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvasTimerDisableScript : MonoBehaviour {
+	private float startTimer;
+	public float timer;
+	private float fadeOutSpeed;
+
+	// Use this for initialization
+	void Start () {
+		startTimer = Time.time;
+		fadeOutSpeed = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTimer > timer) {
+			this.GetComponent<CanvasGroup> ().alpha = Mathf.SmoothDamp(this.GetComponent<CanvasGroup>().alpha, 0f,ref fadeOutSpeed,1f);
+			if (this.GetComponent<CanvasGroup> ().alpha < 0.001f)
+				this.gameObject.SetActive (false);
+		}
+			
+	}
+}
