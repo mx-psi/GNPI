052d2cbdeaca43cdb046fa6b9a41b15b9b929284
Francisco J. Morales <fjmoralespiqueras@gmail.com>
2018-12-16 15:29:56 +0100

    Eliminando cosas

---
+++ Assets/VisibilityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityController : MonoBehaviour {
+	public Renderer rend;
+
+	void Start()
+	{
+		rend = GetComponent<MeshRenderer>();
+		rend.enabled = true;
+	}
+
+	// Toggle the Object's visibility each second.
+	void Update()
+	{
+		// Find out whether current second is odd or even
+		bool oddeven = Mathf.FloorToInt(Time.time) % 2 == 0;
+
+
+		// Enable renderer accordingly
+		rend.enabled = oddeven;
+		Debug.Log (rend.enabled);
+		Debug.Log (rend.isVisible);
+	}
+}
