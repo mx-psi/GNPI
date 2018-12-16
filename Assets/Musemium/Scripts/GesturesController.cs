0d0a93d33d0dbbad63729d288607034536acfbf1
Francisco J. Morales <fjmoralespiqueras@gmail.com>
2018-12-16 20:50:25 +0100

    Update GesturesController.cs

--- Assets/Musemium/Scripts/GesturesController.cs
+++ Assets/Musemium/Scripts/GesturesController.cs
@@ -11,21 +11,96 @@ public class GesturesController : MonoBehaviour {
 	Controller controller; // Main controller
 	private bool zoom = false; // whether we are zooming right now
 
-	private float distBase;
-	private float distRel;
-	private float suavizadoZoom = 5; // zoom smooth parameter
+	private float baseDist;
+	private float relDist;
+	private float zoomSmoothing = 5; // zoom smoothing parameter
 
 	public Camera camera_; //camera
-	private Vector3 posCamara; //camera position
+	private Vector3 cameraPos; //camera position
 
-	private bool movingHand = false; // whether hand is moving
+	private bool handIsMoving = false; // whether hand is moving
 
     /*
      * Initialize controller and camera position
      */
 	void Start() {
 		controller = new Controller();
-		posCamara = camera_.transform.position;
+		cameraPos = camera_.transform.position;
+	}
+
+	/*
+	 *  Map navigation gesture
+	 */
+	void MapNavigation(float x, float y){
+		float leftRighthreshold = 200;
+		float upThreshold = 300;
+		float downThreshold = 100;
+		float step = 0.03f;
+
+		// moving left
+		if ((x < -leftRighthreshold) && (y > downThreshold) && (y < upThreshold)) {
+			cameraPos.x -= step;
+			camera_.transform.position = cameraPos;
+		} 
+		// moving right
+		else if ((x > leftRighthreshold) && (y > downThreshold) && (y < upThreshold)) { 
+			cameraPos.x += step;
+			camera_.transform.position = cameraPos;
+		} 
+		// moving down
+		else if ((y < downThreshold)) {
+			cameraPos.y -= step;
+			camera_.transform.position = cameraPos;
+		}
+		// moving up
+		else if ((y > upThreshold)) {
+			cameraPos.y += step;
+			camera_.transform.position = cameraPos;
+		}
+	}
+
+	/*
+	 *  Next gesture
+	 */
+	void Next(string currentScene){
+		if (currentScene == "paintingScene") { // if scene is paintingScene update color
+			ColorController.NextColor ();
+		} else { // if scene is sculptureScene update color
+			VisibilityController.NextModel ();
+		}
+	}
+
+	/*
+	 *  Previous gesture
+	 */
+	void Previous(string currentScene){
+		if (currentScene == "paintingScene") { // if scene is paintingScene update color
+			ColorController.PreviousColor ();
+		} else { // if scene is sculptureScene update color
+			VisibilityController.PreviousModel ();
+		}
+	}
+
+	/*
+	 *  Zoom gesture
+	 */
+	void Zoom(Hand hand0, Hand hand1){
+		if (zoom == false) {
+			zoom = true; // Start zooming
+			baseDist = hand0.StabilizedPalmPosition.DistanceTo (hand1.StabilizedPalmPosition);
+		} else { // Zoom in
+			relDist = hand0.StabilizedPalmPosition.DistanceTo (hand1.StabilizedPalmPosition) / baseDist;
+			Vector3 camPos = cameraPos;
+			camPos.z /= (relDist - 1) / zoomSmoothing + 1;
+			camera_.transform.position = camPos;
+		}
+	}
+
+	/*
+	 *  Zoom gesture
+	 */
+	void Exit(){
+		SceneManager.LoadScene ("mainScene");
 	}
 
     /*
@@ -35,7 +110,7 @@ public class GesturesController : MonoBehaviour {
 		Frame frame = controller.Frame(); //Frame with hands
 		string currentScene = SceneManager.GetActiveScene ().name; // Current scene name
 
-		// GESTOS CON UNA MANO
+		// One-handed gestures
 
 		if (frame.Hands.Count == 1) {
 			Leap.HandList hands = frame.Hands;
@@ -43,31 +118,12 @@ public class GesturesController : MonoBehaviour {
 
             // Main scene gestures
 			if (currentScene == "mainScene") {
-
-				// MOVIMIENTO POR EL MAPA
-
 				float maxRadius = 50;
-				float threshold = 200;
-				float upThreshold = 300;
-				float downThreshold = 100;
-				float step = 0.03f;
 				float x = hand0.PalmPosition.ToUnity ().x;
 				float y = hand0.PalmPosition.ToUnity ().y;
-
+				// If fist is closed
 				if (hand0.SphereRadius < maxRadius) {
-					if ((x < -threshold) && (y > downThreshold) && (y < upThreshold)) {
-						posCamara.x -= step;
-						camera_.transform.position = posCamara;
-					} else if ((x > threshold) && (y > downThreshold) && (y < upThreshold)) { 
-						posCamara.x += step;
-						camera_.transform.position = posCamara;
-					} else if ((y < downThreshold)) {
-						posCamara.y -= step;
-						camera_.transform.position = posCamara;
-					} else if ((y > upThreshold)) {
-						posCamara.y += step;
-						camera_.transform.position = posCamara;
-					}
+					MapNavigation (x,y);
 				}
 			} 
             // Painting and sculpture gestures
@@ -78,33 +134,27 @@ public class GesturesController : MonoBehaviour {
                 // If hand is tilted
 				if (Mathf.Abs (Vector3.Dot (hand0.PalmNormal.ToUnity (), Vector3.left)) > minDotProd) {
 					if (hand0.PalmVelocity.x < -velocityThreshold) { // if moving left
-						if (movingHand == false) {
-							movingHand = true;
-							if (currentScene == "paintingScene") { // update color
-								ColorController.NextColor ();
-							} else { // change model
-								VisibilityController.NextModel ();
-							}
+						if (handIsMoving == false) {
+							handIsMoving = true;
+							Debug.Log ("Next");
+							Next (currentScene);
 						}
 					} else if (hand0.PalmVelocity.x > velocityThreshold) { // if moving right
-						if (movingHand == false) {
-							movingHand = true;
-							if (currentScene == "paintingScene") { //update color
-								ColorController.PreviousColor ();
-							} else { // change model
-								VisibilityController.PreviousModel ();
-							}
+						if (handIsMoving == false) {
+							handIsMoving = true;
+							Debug.Log ("Previous");
+							Previous (currentScene);
 						}
 					} else {
-						movingHand = false;
+						handIsMoving = false;
 					}
 				} else {
-					movingHand = false;
+					handIsMoving = false;
 				}
 			}
 		}
 
-		// GESTOS CON DOS MANOS
+		// Two-handed gestures
 		else if (frame.Hands.Count == 2) {
 			Leap.HandList hands = frame.Hands;
 			Hand hand0 = hands [0];
@@ -123,43 +173,33 @@ public class GesturesController : MonoBehaviour {
 			bool allExtended = numOfFingers == numExtended;
 
 			if (currentScene == "mainScene") {
-
-				// ZOOM
-
 				float angle1 = Vector3.Angle (Vector3.down, hand0.PalmNormal.ToUnity ());
 				float angle2 = Vector3.Angle (Vector3.down, hand1.PalmNormal.ToUnity ());
 
                 // If both hands are extended facing down
 				if ((angle1 < 45) && (angle2 < 45) && allExtended) {
-					if (zoom == false) {
-						zoom = true; // Start zooming
-						distBase = hand0.StabilizedPalmPosition.DistanceTo (hand1.StabilizedPalmPosition);
-					} else { // Zoom in
-						distRel = hand0.StabilizedPalmPosition.DistanceTo (hand1.StabilizedPalmPosition) / distBase;
-						Vector3 camPos = posCamara;
-						camPos.z /= (distRel - 1) / suavizadoZoom + 1;
-						camera_.transform.position = camPos;
-					}
+					Zoom (hand0, hand1);
 				} else { // Stop zooming
 					if (zoom == true) {
-						posCamara = camera_.transform.position;
+						cameraPos = camera_.transform.position;
 					}
 					zoom = false;
 				}
 			} else {
 
-				// SALIR
+				// exit to main scene
 				Vector index0center = index0.Bone (Leap.Bone.BoneType.TYPE_PROXIMAL).NextJoint;
 				Vector index1center = index1.Bone (Leap.Bone.BoneType.TYPE_PROXIMAL).NextJoint;
 				float  index01distance= index0center.DistanceTo (index1center);
 				float minCrossAngle = 1;
 				float maxCrossAngle = 2;
 				float triggerDist = 45;
-
+				// If both index fingers are extended
 				if (index0.IsExtended && index1.IsExtended && (numExtended == 2)) {
 					float crossAngle = index0.Direction.AngleTo (index1.Direction);
+					// If both fingers are close and approximately form a perpendicular angle
 					if ((crossAngle > minCrossAngle) && (crossAngle < maxCrossAngle) &&  index01distance < triggerDist) {
-						SceneManager.LoadScene ("mainScene");
+						Exit ();
 					}
 				}
 			}
